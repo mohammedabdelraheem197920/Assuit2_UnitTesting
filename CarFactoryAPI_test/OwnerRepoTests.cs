bb55f78adb1faec79d369c48314d50b8c0ae0643
Mohammed <mohammedabdelraheem197920@gmail.com>
2024-05-14 21:23:21 +0300

    Add project files.

---
+++ CarFactoryAPI_test/OwnerRepoTests.cs
@@ -0,0 +1,60 @@
+﻿using CarAPI.Entities;
+using CarFactoryAPI.Entities;
+using CarFactoryAPI.Repositories_DAL;
+using Moq;
+using Moq.EntityFrameworkCore;
+
+namespace CarFactoryAPI_test
+{
+    public class OwnerRepoTests
+    {
+        private Mock<FactoryContext> factoryContextMock;
+
+        private OwnerRepository ownerRepository;
+
+        public OwnerRepoTests()
+        {
+
+            factoryContextMock = new Mock<FactoryContext>();
+
+
+            ownerRepository = new OwnerRepository(factoryContextMock.Object);
+        }
+
+        [Fact]
+        [Trait("Author", "Abdelraheem")]
+        [Trait("Priority", "10")]
+        public void Owner_Id_Ask_Owner10_Return_Owner()
+        {
+            // Arrange
+
+            // Build the mock Data
+            List<Owner> owners = new List<Owner>() { new Owner() { Id = 10 } };
+
+            // setup called DbSets
+            factoryContextMock.Setup(fcm => fcm.Owners).ReturnsDbSet(owners);
+
+            // Act 
+            Owner owner = ownerRepository.GetOwnerById(10);
+
+            // Aassert
+            Assert.NotNull(owner);
+        }
+
+        [Fact]
+        [Trait("Author", "Abdelraheem")]
+        public void Get_all_owner_Return_list_of_owners()
+        {
+
+            List<Owner> owners = new List<Owner>() { new Owner() { Id = 10 } };
+
+            factoryContextMock.Setup(fcm => fcm.Owners).ReturnsDbSet(owners);
+
+
+            List<Owner> owners1 = ownerRepository.GetAllOwners();
+
+
+            Assert.NotNull(owners1);
+        }
+    }
+}
---
+++ Car_Factory_API_test_2/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Car_Factory_API_test_2
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
